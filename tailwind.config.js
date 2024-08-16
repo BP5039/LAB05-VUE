ed39514ce2846347a61dbcbeced2220c7175996f
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2024-08-16 10:08:31 +0700

    FIX tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-import type { Config } from 'tailwindcss'
+/** @type {import { Config } from 'tailwindcss', Config} */
 
 export default {
   content: [
     "/index.html",
-    "/src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -26,4 +26,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+}
